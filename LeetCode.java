777f1b25975d1721de556254168053ac565643b7
Ajay Kumar <ajayk08112001@gmail.com>
2024-03-27 18:17:16 +0530

    Binary Tree level order traversal

--- LeetCode.java
+++ LeetCode.java
@@ -1,5 +1,39 @@
 import java.util.*;
 public class LeetCode {
+        //Problem No: 102 (Level Order Traversal of Binary Trees)
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans=new ArrayList<>();
+        Queue<TreeNode> q=new LinkedList<>();
+        if(root==null)
+            return ans;
+        q.add(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            List<Integer> currlevel=new ArrayList<>();
+            while(size-- >0){
+                TreeNode curr=q.poll();
+                currlevel.add(curr.val);
+                if(curr.left!=null)
+                    q.add(curr.left);
+                if(curr.right!=null)
+                    q.add(curr.right);
+
+
+            }
+            ans.add(currlevel);
+
+        }
+        return ans;
+
+
+    }
+
+
+
+
+
+
+
     //problem No: 1748
     public static int sumOfUnique(int[] nums) {
         HashMap<Integer, Integer> map=new HashMap<>();
